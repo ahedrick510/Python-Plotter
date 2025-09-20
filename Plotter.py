eb6be4e8925096f406b25e1287f5f6f13c1d2e14
Alex-Hedrick <alexander.hedrick@colorado.edu>
2025-09-19 18:32:09 -0600

    pickle testing

--- Plotter.py
+++ Plotter.py
@@ -23,15 +23,15 @@ legend_fontsize = False
 PlotData = Plotter(foldername, verbose, title, xlabel, ylabel1, ylabel2, xlimits=xlimits, ylimits1=ylimits1, ylimits2=ylimits2, legend=legend, 
                    figsize=figsize, title_fontsize=title_fontsize, label_fontsize=label_fontsize, tick_fontsize=tick_fontsize, legend_fontsize=legend_fontsize)
 
-# # Run this if you want to plot from a pickle file
-# PlotData.plot_pickle()
+# Run this if you want to plot from a pickle file
+PlotData.plot_pickle()
 
 # # Run these lines if you want to select data and plot from scratch
 # PlotData.select_data()
 # PlotData.select_columns_twin_axes()
 # PlotData.plot_data()
 
-# Run these lines if you want to select data and plot from scratch, single y-axis
-PlotData.select_data()
-PlotData.select_columns_single_axis()
-PlotData.plot_data()
+# # Run these lines if you want to select data and plot from scratch, single y-axis
+# PlotData.select_data()
+# PlotData.select_columns_single_axis()
+# PlotData.plot_data()
