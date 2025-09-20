3b5f7eb28f339629e144bc753a12c1ad7cea18f8
Alex-Hedrick <alexander.hedrick@colorado.edu>
2025-09-19 18:29:58 -0600

    update for testing

--- Plotter.py
+++ Plotter.py
@@ -26,12 +26,12 @@ PlotData = Plotter(foldername, verbose, title, xlabel, ylabel1, ylabel2, xlimits
 # # Run this if you want to plot from a pickle file
 # PlotData.plot_pickle()
 
-# Run these lines if you want to select data and plot from scratch
-PlotData.select_data()
-PlotData.select_columns_twin_axes()
-PlotData.plot_data()
-
-# # Run these lines if you want to select data and plot from scratch, single y-axis
+# # Run these lines if you want to select data and plot from scratch
 # PlotData.select_data()
-# PlotData.select_columns_single_axis()
+# PlotData.select_columns_twin_axes()
 # PlotData.plot_data()
+
+# Run these lines if you want to select data and plot from scratch, single y-axis
+PlotData.select_data()
+PlotData.select_columns_single_axis()
+PlotData.plot_data()
